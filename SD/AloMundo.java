31d3805aa235ae0fea6ee232961d967bddbcc325
cariasdaniel <d.carias@ymail.com>
2018-10-20 17:09:07 -0300

    Add files via upload

--- SD/AloMundo.java
+++ SD/AloMundo.java
@@ -2,5 +2,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface AloMundo extends Remote {
-	String digaAloMundo() throws RemoteException;
+	String digaAloMundo() throws Exception;
 }
--- SD/AloMundoServidor.java
+++ SD/AloMundoServidor.java
@@ -1,16 +1,51 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.io.*; 
 
 public class AloMundoServidor implements AloMundo {
+	private static int count, fila = 0;
+	private static boolean lock;
+	
 	public AloMundoServidor() {}
 
-	public String digaAloMundo() {
-		System.out.println("Chamada de aplicacao Cliente recebida!");
-		return "Agora nao tem desculpa. Tu jah sabe RMI. Bora programar! Beleza?";
+	public String digaAloMundo() throws Exception {
+		System.out.println("Chamada de aplicacao Cliente recebida! " + count);
+		/*fila++;
+		if(lock){
+			try { 
+				wait();
+			} catch (InterruptedException e)  {
+				Thread.currentThread().interrupt(); 
+				//Log.error("Thread interrupted", e); 
+			}
+		}
+		lock = true;
+		*/
+		File file = new File("C:\\Users\\Daniel\\Documents\\Facul\\SD\\SD-18.2-master\\SD\\test.txt"); 
+		
+		BufferedReader reader = new BufferedReader(new FileReader(file)); 
+
+		String text = "", line; 
+		while ((line = reader.readLine()) != null) {
+			text = text + "\n" + line; 
+		} 
+		
+		count++;
+		Thread.sleep(5000);
+		
+		// fila--;
+		// notify();
+		// if(fila==0) {	
+			// lock = false;
+		// }		
+		
+		return text;
 	}
 
 	public static void main(String args[]) {
+		count = 0;
+		lock = false;
 		try {
 			AloMundoServidor obj = new AloMundoServidor();
 			AloMundo stub = (AloMundo) UnicastRemoteObject.exportObject(obj, 0);
